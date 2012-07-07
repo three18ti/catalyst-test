71b3a1bee7bf3779757709d85c932e2231617986
three18ti <three18ti@gmail.com>
2012-07-07 11:40:46 -0600

    also causes 500 error under admin

--- lib/My/Controller/Admin.pm
+++ lib/My/Controller/Admin.pm
@@ -2,6 +2,8 @@ package My::Controller::Admin;
 use Moose;
 use namespace::autoclean;
 
+use My::Validate::User;
+
 BEGIN {extends 'Catalyst::Controller'; }
 
 =head1 NAME
@@ -43,6 +45,45 @@ sub add :Local {
             password    => $c->req->params->{'password'},
         }
     );
+}
+
+sub create :Local {
+    my ($self, $c) = @_;
+    
+    # Just display the form if this isn't a POST request.
+    return 1 if $c->req->method ne 'POST';
+
+    # Validate fields
+    my $v = My::Validate::User->new();
+    
+#    FIELD:
+#    for my $field ( qw(name username email password) ) {
+#        next FIELD if $v->is_valid($field => $c->req->param->{$field});
+#        
+#        $c->flash(error => 'The ' . $field . ' you entered is not valid.');
+#    }
+
+    if ( $c->req->params->{password} ne $c->req->params->{confirm_password} ) {
+        $c->flash(error => 'The passwords you entered do not match.');
+    }
+    
+    # If there was a form error, redirect back to the form and end this request.
+    if ($c->flash->{error}) {
+        # Save some form values.
+        $c->flash($_ => $c->req->param->{$_}) for qw(name username email);
+        
+        $c->res->redirect('/admin/create');
+        $c->detach();
+    }
+    
+    my $user = $c->model('DB')->resultset('User')->create({
+        name     => $c->req->param->{name},
+        username => $c->req->param->{username},
+        email    => $c->req->param->{email},
+        password => $c->req->param->{password},
+    });
+    
+    $c->res->redirect('/admin/user/' . $user->id . '/read');
 }
 
 =head1 AUTHOR
